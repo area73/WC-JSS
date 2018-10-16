bf009085d97c30592955f6a08dae8688a4ba1ed6
rerades <rerades@siete3.com>
2018-10-17 00:49:26 +0200

    WIP: Media Qery start building

--- tools/mediaQuery.js
+++ tools/mediaQuery.js
@@ -1,3 +1,75 @@
-
 // Based on https://github.com/sass-mq/sass-mq
 
+/**
+ *
+ * @param from {number || String } From: this breakpoint (inclusive)
+ * @param until {number || String } Until: that breakpoint (exclusive)
+ * @param and
+ * @param mediaType
+ * @param breakpoints
+ * @param props
+ */
+const mq = ({from,until,and,mediaType,breakpoints}, props) => {
+
+};
+
+{
+  $min-width: 0;
+  $max-width: 0;
+  $media-query: '';
+
+  // From: this breakpoint (inclusive)
+@if $from {
+  @if type-of($from) == number {
+      $min-width: mq-px2em($from);
+    } @else {
+      $min-width: mq-px2em(mq-get-breakpoint-width($from, $breakpoints));
+    }
+  }
+
+  // Until: that breakpoint (exclusive)
+@if $until {
+  @if type-of($until) == number {
+      $max-width: mq-px2em($until);
+    } @else {
+      $max-width: mq-px2em(mq-get-breakpoint-width($until, $breakpoints)) - .01em;
+    }
+  }
+
+  // Responsive support is disabled, rasterize the output outside @media blocks
+  // The browser will rely on the cascade itself.
+  /*  BOOGIE DON't need it
+    @if $responsive == false {
+      $static-breakpoint-width: mq-get-breakpoint-width($static-breakpoint, $breakpoints);
+      $target-width: mq-px2em($static-breakpoint-width);
+
+      // Output only rules that start at or span our target width
+      @if (
+        $and == false
+              and $min-width <= $target-width
+              and (
+                  $until == false or $max-width >= $target-width
+              )
+              and $media-type != 'print'
+      ) {
+        @content;
+      }
+    }
+  */
+  // Responsive support is enabled, output rules inside @media queries
+  // @else {
+@if $min-width != 0 { $media-query: '#{$media-query} and (min-width: #{$min-width})'; }
+@if $max-width != 0 { $media-query: '#{$media-query} and (max-width: #{$max-width})'; }
+@if $and            { $media-query: '#{$media-query} and #{$and}'; }
+
+  // Remove unnecessary media query prefix 'all and '
+@if ($media-type == 'all' and $media-query != '') {
+    $media-type: '';
+    $media-query: str-slice(unquote($media-query), 6);
+  }
+
+  @media #{$media-type + $media-query} {
+  @content;
+  }
+  // }
+}
