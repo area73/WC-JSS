213778d919a357cba4c6c3c401e1b99f5c2075da
rerades <rerades@siete3.com>
2018-10-28 20:35:01 +0100

    trying to add addons

---
+++ storybook/src/stories/gridSystem.stories.js
@@ -0,0 +1,9 @@
+import '../components/layers/objects/layout';
+import {storiesOf} from '@storybook/polymer';
+
+storiesOf('jss-layout', module)
+.add(
+  'simple with info',
+  () => `<jss-layout ></jss-layout>`
+);
+
