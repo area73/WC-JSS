526229280821b70448ec64e4ceb97f0a26cf9f38
rerades <rerades@siete3.com>
2018-10-28 20:35:01 +0100

    trying to add addons

--- storybook/.storybook/config.js
+++ storybook/.storybook/config.js
@@ -1,12 +1,14 @@
-import { configure, addDecorator } from '@storybook/polymer';
-import { withNotes } from '@storybook/addon-notes';
-import { setConsoleOptions } from '@storybook/addon-console';
+import { configure } from '@storybook/polymer';
+/*
 import { configureViewport } from '@storybook/addon-viewport';
 
-// Console
-setConsoleOptions({panelExclude: []});
-// notes
-addDecorator(withNotes);
+import { setOptions } from '@storybook/addon-options';
+
+setOptions({
+  hierarchyRootSeparator: /\|/,
+});
+*/
+
 // automatically import all files ending in *.stories.js
 const req = require.context('../src/stories', true, /.stories.js$/);
 function loadStories() {
@@ -14,7 +16,3 @@ function loadStories() {
 }
 
 configure(loadStories, module);
-
-
-
-
