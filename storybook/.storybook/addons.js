19d058185e3ecdd29bd367f35250c077a50f3347
rerades <rerades@siete3.com>
2018-10-28 20:35:01 +0100

    trying to add addons

---
+++ storybook/.storybook/addons.js
@@ -0,0 +1,4 @@
+// import '@storybook/addon-info/register';
+// import '@storybook/addon-options/register';
+// import '@storybook/addon-viewport/register';
+// import '@storybook/addon-actions/register';
