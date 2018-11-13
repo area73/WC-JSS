9e91d5981ac9c65e83867d2133714d02a207d7fa
rerades <rerades@siete3.com>
2018-11-13 08:02:00 +0100

    added addons to storybook

--- storybook/.storybook/config.js
+++ storybook/.storybook/config.js
@@ -1,5 +1,12 @@
-import { configure } from '@storybook/polymer';
+import { configure, addDecorator } from '@storybook/polymer';
+import { withNotes } from '@storybook/addon-notes';
+import { setConsoleOptions } from '@storybook/addon-console';
+import { configureViewport } from '@storybook/addon-viewport';
 
+// Console
+setConsoleOptions({panelExclude: []});
+// notes
+addDecorator(withNotes);
 // automatically import all files ending in *.stories.js
 const req = require.context('../src/stories', true, /.stories.js$/);
 function loadStories() {
@@ -7,3 +14,7 @@ function loadStories() {
 }
 
 configure(loadStories, module);
+
+
+
+
