aa9827ff0c3085708602a70aed167e7a1857fdea
rerades <rerades@siete3.com>
2018-11-14 08:03:26 +0100

    remove old demo add new addons Update dependences

--- README.md
+++ README.md
@@ -7,15 +7,6 @@ To make it easier for developers, every measure will be in pixels and the output
 upon config rules (px, rem, em, %) 
 ****
 
-## DEPRECATED: Demo
-To launch the demo, from project root:
-
-```bash
-cd ./demo 
-npm start
-```
-This demo uses webpack to pack es6 modules.
-
 ## TDD
 We use TDD in our development. To run test:
  ````bash
--- storybook/.storybook/config.js
+++ storybook/.storybook/config.js
@@ -2,11 +2,18 @@ import { configure, addDecorator } from '@storybook/polymer';
 import { withNotes } from '@storybook/addon-notes';
 import { setConsoleOptions } from '@storybook/addon-console';
 import { configureViewport } from '@storybook/addon-viewport';
+import { withInfo } from '@storybook/addon-info';
 
+// Decorators
+// ----------
+// addDecorator(withInfo);  // TODO: not working
+addDecorator(withNotes);
+
+// Options
+// -------
 // Console
 setConsoleOptions({panelExclude: []});
-// notes
-addDecorator(withNotes);
+
 // automatically import all files ending in *.stories.js
 const req = require.context('../src/stories', true, /.stories.js$/);
 function loadStories() {
